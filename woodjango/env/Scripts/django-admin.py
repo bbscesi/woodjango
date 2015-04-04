3452cf506826f4b435ed22afe1c4037f6c20649a
Bruno M. BRITO <bgmbrito@gmail.com>
2015-04-04 14:11:51 +0200

    initial version

---
+++ woodjango/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Académique\CESi\RILA\Projet - Fil rouge\Sources\WoodApp2015\woodjango\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
